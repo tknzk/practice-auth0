9e8eb83ff0f4adc2e84d192e869c4290fa8e8d0a
tknzk <tkm.knzk@gmail.com>
2019-09-19 16:03:13 +0900

    g controller auth0, home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home#show'
+  get 'home/show'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # omni-auth
+  get 'auth/auth0/callback' => 'auth0#callback'
+  get 'auth/failure' => 'auth0#failure'
+
+  # api
   get 'api/private' => 'private#private'
   get 'api/private-scoped' => 'private#private_scoped'
 end
