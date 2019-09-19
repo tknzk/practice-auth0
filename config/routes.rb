614e09a5949fc1a6ff29d723969676e4cc1556f9
tknzk <tkm.knzk@gmail.com>
2019-09-19 16:05:21 +0900

    g controller dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   root 'home#show'
   get 'home/show'
+  get 'dashboard/show'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
